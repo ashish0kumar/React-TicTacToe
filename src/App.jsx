91021051dcb1a1d3dcdba8b46ed19e5635a8d0a2
Ashish Kumar <ashishkumar901336@gmail.com>
2024-07-16 22:21:37 +0530

    deriving state from props and avoiding unecessary state management

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,49 @@ import Log from "./components/Log";
 import { useState } from "react"
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
+    setGameTurns((prevTurns) => {
+      let curretPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        curretPlayer = "O";
+      }
+
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: curretPlayer},
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player 
+            initialName="Player 1" 
+            symbol="X" 
+            isActive={activePlayer === "X"} 
+          />
+          <Player 
+            initialName="Player 2" 
+            symbol="O" 
+            isActive={activePlayer === "O"} 
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+
+        <GameBoard 
+          onSelectSquare={handleSelectSquare} 
+          turns={gameTurns}          
+        />
       </div>
+
       <Log />
     </main>
   )
